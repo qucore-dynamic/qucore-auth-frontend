d79ab5cf1de1eae1e1992f0cb16031f282920a15
Artem Zhyto <artemzhr11@gmail.com>
2026-03-31 19:49:57 +0300

    update(ath): rewrites in next.config.ts have been removed and will be moved to the Nginx proxy config

--- next.config.ts
+++ next.config.ts
@@ -15,15 +15,6 @@ const nextConfig: NextConfig = {
     externalDir: true,
   },
 
-  async rewrites() {
-    return [
-      {
-        source: '/api/auth/:path*',
-					destination: `${AUTH_URL}/:path*`,
-      },
-    ]
-  },
-
   images: {
     remotePatterns: [
       {
@@ -42,7 +33,7 @@ const nextConfig: NextConfig = {
         {
           key: 'Content-Security-Policy',
           value:
-            'frame-src \'self\' https://www.google.com; script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://www.google.com https://www.gstatic.com; connect-src \'self\';',
+            "frame-src 'self' https://www.google.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com; connect-src 'self';",
         },
       ],
     },
